f5609088fe661035f8b1c88462778b8692b4daac
mattchew1010 <mattchewiscool@gmail.com>
2023-05-24 02:33:14 -0400

    model runner

--- ai.py
+++ ai.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
 from model import Model
 
-noise_dim = 200
+noise_dim = 128
 batch_size = 32
-ds = tf.keras.utils.image_dataset_from_directory("./Data/Avatars", image_size=(64,64), batch_size=batch_size, labels=None).take(750).cache().prefetch(tf.data.AUTOTUNE)
+ds = tf.keras.utils.image_dataset_from_directory("./Data/Avatars", image_size=(64,64), batch_size=batch_size, labels=None).take(3000).cache().prefetch(tf.data.AUTOTUNE)
 
 Model = Model(noise_dim)
-Model.compile()
 
-
-
+Model.discriminator(Model(tf.random.normal([batch_size, noise_dim])))
+print(Model.generator.model.summary())
+print(Model.discriminator.model.summary())
 
 Model.fit(ds, epochs=1000, batch_size=batch_size)
